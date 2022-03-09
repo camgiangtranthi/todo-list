357e25b2469be569bf3812417cee38795f197259
Cam Giang <98574958+camgiangtranthi@users.noreply.github.com>
2022-03-09 22:59:35 +0700

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,1 +1,11 @@
-
+export default function logger(reducer) {
+    return (prevState, action, args) => {
+        console.group(action)
+        console.log('Prev state: ', prevState)
+        console.log('Arguments: ', args)
+        const nextState = reducer(prevState, action, args)
+        console.log('nextState: ', nextState)
+        console.groupEnd()
+        return nextState
+    }
+}
